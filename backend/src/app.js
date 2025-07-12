9d9902fdfa590ffee45158fe79037e785ad656d4
Isaac Mosiah <isaacmosiah@alu.ufc.br>
2025-07-12 15:33:56 -0300

    Add: Criei pasta views com páginas estáticas para tratar rotas não implementadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-app.use(express.static('src'));
+
 userRoutes(app);
 adminRoutes(app);
+
+// Middleware to serve static files - if needed
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../src/views/index.html'));
+});
+app.get("/{*any}", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '../src/views/404.html'));
+});
 
 module.exports = app;
