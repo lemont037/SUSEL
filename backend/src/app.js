562d37c9e4778ded4e69d89e285aab6c3862a16a
Isaac Mosiah <isaacmosiah@alu.ufc.br>
2025-07-15 13:15:16 -0300

    Add: Adicionei CORS em preparação para integração com backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 userRoutes(app);
 adminRoutes(app);
